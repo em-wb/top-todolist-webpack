17d4665f0778884fc70ca375e103d1cc50a548a9
em-wb <131539794+em-wb@users.noreply.github.com>
2023-12-11 18:09:09 +0100

    Move functions outside of project object

--- src/project.js
+++ src/project.js
@@ -1,20 +1,25 @@
 export const allProjects = [];
 
+function addToAllProjects() {
+  allProjects.push(this);
+}
+
+function addTodo(todo) {
+  if (todo.assignedProjects.some((project) => project === this.title)) {
+    this.todosArray.push(todo);
+  }
+}
+
 export default function createProject(title, description, colour) {
   const project = {
     title: title,
     description: description,
     colour: colour,
     todosArray: [],
-    addToAllProjects: function () {
-      allProjects.push(this);
-    },
-    addTodo: function (todo) {
-      if (todo.assignedProjects.some((project) => project === this.title)) {
-        this.todosArray.push(todo);
-      }
-    },
+    addToAllProjects: addToAllProjects,
+    addTodo: addTodo,
   };
+
   project.addToAllProjects();
   return project;
 }
