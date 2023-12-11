d339eff9b51c235633b097a4bbacf28c08e17cb4
em-wb <131539794+em-wb@users.noreply.github.com>
2023-12-11 17:25:12 +0100

    Add test objects

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
-import todo from "./todo";
-import createProject from "./project";
+import createProject, { allProjects } from "./project";
+import createTodo from "./todo";
 
-const { defaultProject } = createProject();
-todo(defaultProject);
+const defaultProject = createProject(
+  "All Projects",
+  "Description of my project",
+  "blue"
+);
+
+const todo1 = createTodo(
+  "My first to do",
+  "I need to do something",
+  "01/03/2024",
+  "high",
+  [defaultProject.title],
+  false
+);
+
+allProjects.forEach((project) => {
+  project.addTodo(todo1);
+});
+
+console.log("todo", todo1);
+console.log("default", defaultProject);
+console.log("allprojects", allProjects);
