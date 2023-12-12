3b6819210f18b4a46ff2c96eecee9640b17a2fcd
em-wb <131539794+em-wb@users.noreply.github.com>
2023-12-12 16:48:57 +0100

    Add event listener for dom content

--- src/index.js
+++ src/index.js
@@ -1,25 +1,31 @@
 import createProject, { allProjects } from "./project";
 import createTodo from "./todo";
+import { renderCoreUI } from "./ui";
+import "./styles.css";
 
-const defaultProject = createProject(
-  "All Projects",
-  "Description of my project",
-  "blue"
-);
+document.addEventListener("DOMContentLoaded", () => {
+  const defaultProject = createProject(
+    "All Projects",
+    "Description of my project",
+    "blue"
+  );
 
-const todo1 = createTodo(
-  "My first to do",
-  "I need to do something",
-  "01/03/2024",
-  "high",
-  [defaultProject.title],
-  false
-);
+  const todo1 = createTodo(
+    "My first to do",
+    "I need to do something",
+    "01/03/2024",
+    "high",
+    [defaultProject.title],
+    false
+  );
 
-allProjects.forEach((project) => {
-  project.addTodo(todo1);
+  allProjects.forEach((project) => {
+    project.addTodo(todo1);
+  });
+
+  console.log("todo", todo1);
+  console.log("default", defaultProject);
+  console.log("allprojects", allProjects);
+
+  renderCoreUI();
 });
-
-console.log("todo", todo1);
-console.log("default", defaultProject);
-console.log("allprojects", allProjects);
