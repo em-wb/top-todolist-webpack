405c55bfbfd87f77147f152a31da8ef9a989b0ab
em-wb <131539794+em-wb@users.noreply.github.com>
2023-12-12 22:43:03 +0100

    Add header and footer dom elements

--- src/ui.js
+++ src/ui.js
@@ -3,16 +3,52 @@ function getContentEl() {
   return content;
 }
 
-function createElement(tag, className, parent) {
+function createElement(tag, classList, parent, text) {
   const newElement = document.createElement(tag);
-  newElement.classList.add(className);
+  classList = Array.isArray(classList)
+    ? newElement.classList.add(...classList)
+    : newElement.classList.add(classList);
+  newElement.textContent = text;
   parent.append(newElement);
   return newElement;
 }
 
-export function renderCoreUI() {
+export function renderHeader() {
   const header = createElement("header", "header", content);
-  console.log("hello");
+  const titleCtr = createElement("div", "titleCtr", header);
+  const title = createElement("h1", "title", titleCtr, "All tasks");
+  const projectDesc = createElement(
+    "h2",
+    "projectDesc",
+    titleCtr,
+    "All your tasks in one list"
+  );
+  const ellipsis = createElement(
+    "i",
+    ["fa-solid", "fa-ellipsis-vertical"],
+    header
+  );
+}
+
+const iconList = [
+  { classes: ["fa-regular", "fa-list"], text: "All tasks" },
+  { classes: ["fa-regular", "fa-star"], text: "Today" },
+  { classes: ["fa-regular", "fa-folder-tree"], text: "All Lists" },
+  { classes: ["fa-regular", "fa-user"], text: "Profile" },
+];
+
+function getIconMenu(iconsDiv) {
+  iconList.forEach((icon) => {
+    createElement("i", icon.classes, iconsDiv);
+    createElement("small", "menuText", iconsDiv, icon.text);
+  });
+}
+
+export function renderFooter() {
+  const footer = createElement("footer", null, content);
+  const iconsDiv = createElement("div", "iconsDiv", footer);
+  const logo = createElement("h3", "logo", footer, "Absolistly");
+  getIconMenu(iconsDiv);
 }
 
 export function rendersproject() {}
