91df440472464608d3ec7e492afb2953226ef3a8
em-wb <131539794+em-wb@users.noreply.github.com>
2023-12-13 18:19:37 +0100

    Edit core app build and styles

--- src/index.js
+++ src/index.js
@@ -1,32 +1,58 @@
 import createList, { allLists } from "./lists";
 import createTask from "./tasks";
-import { renderHeader, renderFooter, renderList } from "./ui";
+import { renderHeader, renderFooter, renderList, renderCoreApp } from "./ui";
 import "./styles.css";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const allTasks = createList(
-    "All Tasks",
-    "All your tasks in one list",
-    "blue"
-  );
-
-  renderFooter();
-  renderHeader(allTasks);
+  const listAll = createList("All Tasks", "All your tasks in one list", "blue");
 
   const task1 = createTask(
     "My first to do",
     "I need to do something",
-    "01/03/2024",
-    "high",
-    [allTasks.title],
+    "01 Mar 24",
+    true,
+    [listAll.title],
+    false
+  );
+
+  const task2 = createTask(
+    "My second to do",
+    "I need to do something",
+    "01 Jan 24",
+    false,
+    [listAll.title],
+    true
+  );
+
+  const task3 = createTask(
+    "Task 3",
+    null,
+    "19 Dec 23",
+    false,
+    [listAll.title],
     false
   );
 
   allLists.forEach((list) => {
     list.addTask(task1);
+    list.addTask(task2);
+    list.addTask(task3);
   });
 
-  console.log("task", task1);
-  console.log("all", allTasks);
+  renderFooter();
+  renderCoreApp(listAll);
+
+  renderHeader(listAll);
+
+  console.log("task", task1, task2);
+  console.log("all", listAll);
   console.log("allLists", allLists);
 });
+
+// function addToAllTasks() {
+//   list.tasksArray.forEach((task) => {
+//     allLists.forEach((list) => {
+//       list.addTask(task);
+//     });
+//   });
+// }
