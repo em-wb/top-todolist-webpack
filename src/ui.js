0242955cf3fd4296f1aa5a8e866dd6b5723addce
em-wb <131539794+em-wb@users.noreply.github.com>
2023-12-13 18:19:06 +0100

    Edit core app build and styles

--- src/ui.js
+++ src/ui.js
@@ -5,11 +5,6 @@ const iconList = [
   { classes: ["fa-solid", "fa-user"], text: "Profile" },
 ];
 
-function getContentEl() {
-  const content = document.getElementById("content");
-  return content;
-}
-
 function createElement(tag, classList, parent, text) {
   const newElement = document.createElement(tag);
   classList = Array.isArray(classList)
@@ -51,21 +46,52 @@ export function renderFooter() {
 export function renderCoreApp(list) {
   console.log("render core");
   const coreAppCtr = createElement("div", "coreAppCtr", content);
+  const addNewBtn = createElement(
+    "button",
+    ["addNewBtn", "fa-solid", "fa-plus"],
+    content
+  );
   const tasksCtr = createElement("div", "tasksCtr", coreAppCtr);
-  const completeCtr = createElement("div", "completeCtr", coreAppCtr);
+  const doneCtr = createElement("div", "doneCtr", coreAppCtr);
+  createElement("h4", "doneheader", doneCtr, "Done");
   list.tasksArray.forEach((task) => {
-    const taskItem = createElement("div", "taskItem", tasksCtr);
-    const taskInfoDiv = createElement("div", "taskInfoDiv", taskItem);
-    const listInfoDiv = createElement("div", "listInfoDiv", taskItem);
-    const taskCompleteDiv = createElement("div", "taskCompleteDiv", taskItem);
-    createElement("h4", "taskTitle", taskInfoDiv, task.title);
-    createElement("p", "taskDesc", taskInfoDiv, task.description);
-    createElement("div", "listAssignment", listInfoDiv);
-    createElement("small", "dueDate", taskCompleteDiv, task.dueDate);
-    createElement(
-      "button",
-      ["completeBtn", "fa-solid", "fa-check"],
-      taskCompleteDiv
-    );
+    if (task.completed) {
+      const doneItem = createElement("div", "doneItem", doneCtr);
+      createElement("h5", "done", doneItem, task.title);
+      createElement("small", "done", doneItem, task.dueDate);
+      createElement("div", ["listAssignment", "done"], doneItem);
+      createElement(
+        "button",
+        ["completeBtn", "fa-solid", "fa-check", "done"],
+        doneItem
+      );
+      createElement("hr", "break", doneCtr);
+    }
+  });
+
+  list.tasksArray.forEach((task) => {
+    if (!task.completed) {
+      const taskItem = createElement("div", "taskItem", tasksCtr);
+      const taskInfoDiv = createElement("div", "taskInfoDiv", taskItem);
+      const listInfoDiv = createElement("div", "listInfoDiv", taskItem);
+      const taskCompleteDiv = createElement("div", "taskCompleteDiv", taskItem);
+
+      const taskTitle = createElement(
+        "h4",
+        "taskTitle",
+        taskInfoDiv,
+        task.title
+      );
+      if (task.priority == true) taskTitle.classList.add("priority");
+      createElement("p", "taskDesc", taskInfoDiv, task.description);
+      createElement("div", "listAssignment", listInfoDiv);
+      createElement("small", "dueDate", taskCompleteDiv, task.dueDate);
+      createElement(
+        "button",
+        ["completeBtn", "fa-solid", "fa-check"],
+        taskCompleteDiv
+      );
+      createElement("hr", "break", tasksCtr);
+    }
   });
 }
