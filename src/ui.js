8695e138137edde67f5e95a6a118417faf1dec87
em-wb <131539794+em-wb@users.noreply.github.com>
2023-12-14 16:44:16 +0100

    Add UI for dialog to input new tasks

--- src/ui.js
+++ src/ui.js
@@ -5,14 +5,144 @@ const iconListFooter = [
   { classes: ["fa-solid", "fa-user"], text: "Profile" },
 ];
 
-function createElement(tag, classList, parent, text) {
+function createElement(tag, classList, parentEl, text, attributes) {
   const newElement = document.createElement(tag);
-  classList = Array.isArray(classList)
-    ? newElement.classList.add(...classList)
-    : newElement.classList.add(classList);
+  if (classList) {
+    classList = Array.isArray(classList)
+      ? newElement.classList.add(...classList)
+      : newElement.classList.add(classList);
+  }
   newElement.textContent = text;
-  parent.append(newElement);
+  if (attributes) {
+    attributes.forEach((element) => {
+      newElement.setAttribute(element[0], element[1]);
+    });
+  }
+
+  parentEl.append(newElement);
   return newElement;
+}
+
+const taskDialogElements = [
+  {
+    elements: [
+      ["h1", null, null, "NewTask", [["id", "taskDialogH1"]]],
+      [
+        "button",
+        ["closeBtn", "fa-solid", "fa-xmark"],
+        null,
+        "",
+        [["id", "closeTaskDialog"]],
+      ],
+    ],
+  },
+  {
+    elements: [
+      ["h2", null, null, "Title"],
+      [
+        "input",
+        "inputText",
+        null,
+        "",
+        [
+          ["id", "inputTaskTitle"],
+          ["type", "text"],
+          ["required", ""],
+        ],
+      ],
+    ],
+  },
+  {
+    elements: [
+      ["h2", null, null, "Description (optional)"],
+      [
+        "input",
+        "inputText",
+        null,
+        "",
+        [
+          ["id", "inputTaskDesc"],
+          ["type", "text"],
+        ],
+      ],
+    ],
+  },
+  {
+    elements: [
+      ["h2", null, null, "Due Date"],
+      [
+        "input",
+        "inputDate",
+        null,
+        "",
+        [
+          ["id", "inputTaskDate"],
+          ["type", "date"],
+        ],
+      ],
+    ],
+  },
+  {
+    elements: [
+      ["h2", null, null, "Priority"],
+      ["button", "priorityBtn", null, "Normal", [["id", "normalPriorityBtn"]]],
+      ["button", "priorityBtn", null, "High", [["id", "highPriorityBtn"]]],
+    ],
+  },
+  {
+    elements: [
+      ["h2", null, null, "List"],
+      [
+        "input",
+        null,
+        null,
+        "",
+        [
+          ["id", "dropdownList"],
+          ["list", "listOfLists"],
+        ],
+      ],
+      ["datalist", null, null, "", [, ["id", "listOfLists"]]],
+    ],
+  },
+  {
+    elements: [
+      [
+        "button",
+        "submitBtn",
+        null,
+        "List it",
+        [
+          ["id", "submitTaskBtn"],
+          ["type", "submit"],
+        ],
+      ],
+      ["button", "deleteBtn", null, "Delete it", [["id", "deleteTaskBtn"]]],
+    ],
+  },
+];
+
+function createNewTaskDialog() {
+  const dialog = createElement("dialog", "taskDialog", content, "", [
+    ["id", "taskDialog"],
+  ]);
+  const form = createElement("form", "newTaskForm", dialog, "", [
+    ["id", "newTaskForm"],
+  ]);
+  for (let i = 0; i < 7; i++) {
+    const taskDialogDiv = createElement("div", "dialogDivs", form, "", [
+      ["id", `taskDialogDiv${i}`],
+    ]);
+    if (taskDialogElements[i]) {
+      taskDialogElements[i].elements.forEach((elementArray) => {
+        elementArray[2] = taskDialogDiv;
+        createElement(...elementArray);
+        //PLACEHOLDER... TO LOOP THROUGH LISTS LATER
+      });
+    }
+  }
+  const dropdown = document.getElementById("listOfLists");
+  createElement("option", null, dropdown, "", [["value", "All tasks"]]);
 }
 
 function getIconMenu(menuDiv) {
@@ -54,7 +184,6 @@ function renderCurrentTaskItems(tasksCtr, list) {
       const taskInfoDiv = createElement("div", "taskInfoDiv", taskItem);
       const listInfoDiv = createElement("div", "listInfoDiv", taskItem);
       const taskCompleteDiv = createElement("div", "taskCompleteDiv", taskItem);
-
       const taskTitle = createElement(
         "h4",
         "taskTitle",
@@ -98,11 +227,14 @@ export function renderCoreApp(list) {
   const addNewBtn = createElement(
     "button",
     ["addNewBtn", "fa-solid", "fa-plus"],
-    content
+    content,
+    "",
+    [["id", "addNewBtn"]]
   );
   const tasksCtr = createElement("div", "tasksCtr", coreAppCtr);
   const doneCtr = createElement("div", "doneCtr", coreAppCtr);
   createElement("h4", "doneheader", doneCtr, "Done");
   renderCompletedTaskItems(doneCtr, list);
   renderCurrentTaskItems(tasksCtr, list);
+  createNewTaskDialog();
 }
