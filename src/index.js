338cf929d82180e034997faa13345bd165126e55
em-wb <131539794+em-wb@users.noreply.github.com>
2024-02-15 14:25:23 +0100

    Call render app and load list functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import renderAppUI from "./appUI";
 import { loadListsFromStorage } from "./lists";
 import "./styles.css";
 
 document.addEventListener("DOMContentLoaded", () => {
+  renderAppUI();
   loadListsFromStorage();
 });
