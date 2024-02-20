e50b76d8d4d7b9c8d3007a0d605e3bf5eab53609
em-wb <131539794+em-wb@users.noreply.github.com>
2024-02-20 11:32:56 +0100

    Add submit list event listener

--- src/addNewList.js
+++ src/addNewList.js
@@ -4,6 +4,12 @@ import renderMainForm, {
   renderDelete,
   renderSubmit,
 } from "./addNew";
+import { editedLog } from "./addNewTask";
+import createList, { loadListsFromStorage } from "./list";
+import clearViewCtr from ".";
+
+let edited = false;
+let editedIndex;
 
 export function renderListForm(listToEdit) {
   const viewCtr = getViewCtr();
@@ -11,4 +17,50 @@ export function renderListForm(listToEdit) {
   renderCloseBtn(viewCtr);
   const submitBtn = renderSubmit(mainForm);
   const deleteBtn = renderDelete(mainForm);
+  submitListEL(submitBtn);
+}
+
+// function deleteTaskEL(deleteTaskBtn) {
+//   deleteTaskBtn.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     if (edited) {
+//       deleteTask(taskIndex);
+//     }
+//     clearViewCtr();
+//     addTaskCtrs();
+//     loadTasksFromStorage(1); //currentlist
+//     addTaskEventLis();
+//     edited = false;
+//     taskIndex = null;
+//   });
+// }
+
+// function closeFormEL(closeBtn) {
+//   closeBtn.addEventListener("click", (e) => {
+//     clearViewCtr();
+//     addTaskCtrs();
+//     loadTasksFromStorage(1); //currentlist
+//     addTaskEventLis();
+//     edited = false;
+//     taskIndex = null;
+//   });
+// }
+
+function submitListEL(submitListBtn) {
+  submitListBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (edited) {
+      deleteList(editedIndex);
+    }
+    createList(
+      document.getElementById("inputTitle").value,
+      document.getElementById("inputDesc").value,
+      "blue" //add colour chart
+    );
+    clearViewCtr();
+    loadListsFromStorage();
+    addListELs();
+    edited = false;
+    editedIndex = null;
+  });
 }
